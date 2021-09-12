26688c3a5f129611f556da89140b39af8b4fb6ed
Davi <davisouzag@gmail.com>
2021-09-12 11:59:47 -0300

    Criação do layout base

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <h1>Bem vindo ao aplicativo de notas</h1>
+    <button @click="Adc" class='btnAdc'>Adicionar nova nota</button>
+
+    <div class='list-notes'>
+      <Notas />
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Notas from './components/Notas.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
+  components: {
+    Notas
+  },
+  data() {
+    return {
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    }
+  },
+  methods: {
+    Adc() {
+      alert('teste')
     }
   }
+}
+</script>
+
+<style lang="scss">
+#app {
+  h1 {
+    font: 300 28px 'Roboto', sans-serif;
+    text-align: start;
+  }
+}
+
+.btnAdc {
+  background: #AD7AC6;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  border: none;
+  color: #fff;
+  width: 200px;
+  height: 50px;
+  font: 100 18px 'Roboto', sans-serif;
+  cursor: pointer;
+  transition: filter 0.2s;
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+}
+
+.list-notes {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/Notas.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="content">
+    <div class="note">
+        asdaasdasd
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notas",
+};
+</script>
+
+<style lang="scss">
+.content {
+    max-width: 1120px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.note {
+  background: #cac8c8;
+  border-radius: 8px;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.04);
+  padding: 24px;
+
+  margin-top: 32px;
+}
+</style>
